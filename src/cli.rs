1fb07ea23d2992bdf1abeab0c45c9eaa312f10f9
ivan-nosar <wowgm@yandex.ru>
2026-05-27 23:43:47 +0200

    Initial implementation of argument parser

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use crate::constants;
 
 pub struct RawArgs<'a> {
@@ -20,4 +21,50 @@ pub fn parse_raw_args(args: &Vec<String>) -> Result<RawArgs, &'static str> {
         }),
         [] => Err(constants::strings::errors::COMMAND_NOT_PROVIDED_MESSAGE),
     }
+}
+
+
+
+pub enum ArgType {
+    Flag,
+    AssignedValue,
+}
+
+// pub struct ArgRegistration<'a> {
+//     name: &'a str,
+//     arg_type: ArgType,
+// }
+
+pub fn parse_registered_args(args: &[String], registered_args: HashMap<String, ArgType>)
+    -> Result<HashMap<&String, String>, String> {
+    // Args missed in `args` will be silently skipped; Caller should decide what to do with it.
+    // Unregistered args that were present in `args` will be skipped as well.
+
+    let mut result: HashMap<&String, String> = HashMap::new();
+
+    let mut index = 0;
+    while index < args.len() {
+        let item = &args[index];
+
+        match registered_args.get(item) {
+            Some(&ArgType::Flag) => {
+                result.insert(item, String::from("true"));
+            }
+            Some(&ArgType::AssignedValue) => {
+                if index == args.len() - 1 {
+                    return Err(
+                        format!("'{item}': {}", constants::strings::errors::COMMAND_NOT_PROVIDED_MESSAGE)
+                    );
+                }
+
+                let arg_value = &args[index + 1];
+                result.insert(item, arg_value.clone());
+            }
+            None => continue
+        }
+
+        index += 1;
+    }
+
+    Ok(result)
 }
--- src/constants/strings/errors.rs
+++ src/constants/strings/errors.rs
@@ -2,6 +2,7 @@
 pub const CLI_ARGS_INVALID_MESSAGE: &str = "Command line arguments are invalid. Executable path/alias is expected to be first in the args list.";
 pub const COMMAND_NOT_PROVIDED_MESSAGE: &str = "Command is not provided. Please run `hants help` to get the full list of commands supported by hants.";
 pub const COMMAND_IS_NOT_SUPPORTED_MESSAGE: &str = "Command is not supported. Please run `hants help` to get the full list of commands supported by hants.";
+pub const AAA: &str = "An argument is expected to have a value, but none was provided.";
 
 // Base64 error message.
 pub const BASE64_ACTION_NOT_PROVIDED_MESSAGE: &str = "Action is not provided. Please run `hants help` to get the full list of commands supported by hants.";
